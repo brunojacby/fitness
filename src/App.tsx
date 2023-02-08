5fc51f371f2b166d578504b5f752f22ed647a22f
Bruno Jacby <brunojck@hotmail.com>
2023-02-08 21:19:21 +0000

    adc Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import Navbar from "@/scenes/navbar"
+import { useState } from "react"
+
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  AboutUs = "contactus"
+}
+
 function App() { 
+const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
   return (
-    <div className="app">app</div>
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
   )
 }
 
